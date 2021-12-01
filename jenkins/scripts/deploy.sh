ad09fdf8da18284e9dcb2f125ef8e6119eff0b1c
FredoNg <alfredng92@gmail.com>
2021-12-02 00:02:16 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 192.168.220.130:80:80 --name my-apache-php-app -v /home/fred/UITesting03/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
