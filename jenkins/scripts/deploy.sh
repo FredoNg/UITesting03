b3ff001bd7ce78b36344149c35246bed17fa7355
FredoNg <alfredng92@gmail.com>
2021-12-02 18:47:18 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
+docker run -d -p 10.1.5.22:8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://10.1.5.22:8000 to see your PHP application in action.'
+echo 'Visit http://18.222.75.64:8000 to see your PHP application in action.'
