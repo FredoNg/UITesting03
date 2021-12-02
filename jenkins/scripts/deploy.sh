79f055d28044456f848350b694f7499c3a38ec62
FredoNg <alfredng92@gmail.com>
2021-12-02 14:15:52 +0800

    asdfghjk

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
+docker run -d -p 8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.220.130:80 to see your PHP application in action.'
-
-jenkins/scripts/deploy.sh
+echo 'Visit http://192.168.220.130:8000 to see your PHP application in action.'
--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://172.20.0.3:80";
+	String url = "http://192.168.220.130:8000";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
