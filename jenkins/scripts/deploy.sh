8f00ca275cfb188e6b4e9a7b99e7595505bf7567
FredoNg <alfredng92@gmail.com>
2021-12-02 12:48:12 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 172.20.0.3:80:80 --name my-apache-php-app -v /home/fred/UITesting03/src:/var/www/html php:7.2-apache
+docker run -d -p 8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://192.168.220.130:80 to see your PHP application in action.'
 
+jenkins/scripts/deploy.sh
